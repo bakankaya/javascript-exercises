cb00dd89c301fe2d97f73b87257a468ee2f9d46d
bakankaya <batakankarakaya@gmail.com>
2022-12-22 16:14:27 +0300

    second solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let outcome = '';
+    for (let i =0; i < num; i++){
+        outcome += string;
+    }
+    return outcome;
 };
 
 // Do not edit below this line
