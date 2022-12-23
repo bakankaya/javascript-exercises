2d890344e66e22288c4ad64a636ad5dc90cc8a71
bakankaya <batakankarakaya@gmail.com>
2022-12-23 12:09:04 +0300

    solution for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let arr = [];
+    let outcome = "";
+    for (let i= 0;i<word.length; i++){
+        arr.unshift(word.charAt(i));
+    }
+    for (let j=0; j<arr.length;j++){
+        outcome +=arr[j];
+    }
+    return outcome;
 };
 
 // Do not edit below this line
