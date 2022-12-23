23620d09e0d320d7788ab93724e535d737ee7f87
bakankaya <batakankarakaya@gmail.com>
2022-12-23 22:08:17 +0300

    05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    if (typeof num1 !== "number" || typeof num2 !== "number") return 'ERROR';
+    if (num1 < num2){
+        let fsum = 0;
+        for (let i = num1; i <= num2; i++){
+            fsum += i;
+        }
+        return fsum;
+    } else {
+        let fsum = 0;
+        for (let i = num2; i <= num1; i++){
+            fsum += i;
+        }
+        return fsum;
+    }
 
 };
 
